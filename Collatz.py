2920782c5e6f8a8918ecea0f260d397cece88a86
Maverick Reynolds <mcreynolds02@gmail.com>
2023-06-11 13:54:34 -0400

    first commit

---
+++ Collatz.py
@@ -0,0 +1,90 @@
+'''
+So This is the perfect hello world program for data visualization / computer science
+Taking this old thing and making it new again
+
+Maverick Reynolds
+06.10.2023
+'''
+
+import streamlit as st
+import matplotlib.pyplot as plt
+from PIL import Image
+
+@st.cache_data
+def collatz(z):
+    steps = [z]
+
+    while z != 1:
+        if z % 2 == 0:
+            z //= 2
+        else:
+            z = 3*z + 1
+
+        steps.append(z)
+        
+    return steps
+
+@st.cache_data
+def collatz_stops(n):
+    return [len(collatz(i)) - 1 for i in range(1, n + 1)]
+
+def main():
+    # Information about the app
+    st.title('Interactive Collatz')
+    st.write('Made by Maverick Reynolds')
+
+    st.subheader('About')
+    st.write('This interactive allows a user to input a number and see the Collatz sequence for that number. The sequence is defind by a simple relation as shown below:')
+
+    st.write('''
+    $$
+    f(n) =
+    \\begin{cases}
+    n/2, & \\text{if } n \\text{ is even} \\\\
+    3n + 1, & \\text{if } n \\text{ is odd}
+    \\end{cases}
+    $$
+    ''')
+    
+    st.write('The Collatz conjecture states that for any positive integer, the sequence will always reach *1*. Use the sidebar to enter a number and see the sequence displayed.')
+
+    # Make a sidebar for input
+    st.sidebar.title('Enter an Integer:')
+    z = st.sidebar.number_input('', 1, step=1)
+
+    # Display the sequence
+    st.subheader('The Collatz Sequence for *' + str(z) + '*:')
+
+    coll_output = collatz(z)
+
+    # Display the graph
+    st.line_chart(coll_output)
+
+    # Raw List
+    if st.checkbox('Show raw list'):
+        st.write(coll_output)
+
+    st.header('Importance of Iterations')
+    st.write('The number of iterations it takes to reach *1* is called the stopping time. The stopping time for *' + str(z) + '* is *' + str(len(coll_output) - 1) + '*. As the value of the input increases, the stopping time generally tends to increase. The stopping time for the first 1000 positive integers is shown below:')
+
+    # Display the graph using matplotlib (use image)
+    stops_img = Image.open('stopping_times_100K.png')
+    st.image(stops_img, use_column_width=True)
+
+    st.subheader('Interesting Observations')
+    st.write('As the numbers increase, the trivial lower bound follows the curve $y=log_2(x)$ because the quickest reduction would occur if the number passed through is a power of *2* so that at every iteration we are dividing by *2* to get to *1*. The upper bound is much harder to predict. and is still an open problem.')
+    st.write('In addition, the graph shows some very peculiar curves which are quite satisfying to look at.')
+
+    st.header('Other Resources')
+    st.write('For more information on the Collatz conjecture, check out the following links:')
+    st.write('**Wikipedia** https://en.wikipedia.org/wiki/Collatz_conjecture')
+    st.write('**Numberphile** https://www.youtube.com/watch?v=5mFpVDpKX70')
+    st.write('**Quanta Magazine** https://www.quantamagazine.org/why-mathematicians-still-cant-solve-the-collatz-conjecture-20200922')
+
+    
+
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ README.md
@@ -0,0 +1,20 @@
+# Collatz Conjecture Visualizer
+
+This project was made as both a hello world data visualizations project and a great opportunity to connect with streamlit for interactive opportunities.
+
+This was a rather simple project for me to create, yet I felt it necessary as it established my familiarity with both Streamlit, Medium, and helped me to create an article on my own website.
+
+## How it Works
+
+The Streamlit interactive is based off of Collatz.py, and can be accessed at THIS LINK. The user can enter an integer into the side bar and view the corresponding graph appear which shows the progression of the value as the relation is recursively applied until it eventually subsides at 1. Below is more information about the interesting properties and patterns of the Collatz relation as well as other resources for additional learning
+
+## Other Information
+
+Check out how I made it:
+- MEDIUM
+- MAVREYN
+
+Here are the additional links that can be found in the Streamlit
+- [Wikipedia](https://en.wikipedia.org/wiki/Collatz_conjecture)
+- [Numberphile](https://www.youtube.com/watch?v=5mFpVDpKX70)
+- [Quanta](https://www.quantamagazine.org/why-mathematicians-still-cant-solve-the-collatz-conjecture-20200922)
